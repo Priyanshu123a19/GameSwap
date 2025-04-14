99e8d71a519cde4d60da369870ddd7bda709479c
H@des2004 <p.priyanshuspn@gmail.com>
2025-04-14 14:20:51 +0530

    navbarFix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css"; // Assuming this contains your background styles
 import MainNavBar from "./MainNavBar"; // Import the MainNavBar component
-
+import SubNavBar from "./SubNavBar"; // Import the SubNavBar component
 function App() {
   return (
     <div id="root">
       <MainNavBar /> {/* Add the black strip component here */}
+      <SubNavBar /> {/* Add the SubNavBar component here */}
       <div className="content">
       </div>
     </div>
